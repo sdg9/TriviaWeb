14fff35c522335a8fd9e159a3a576acd967fab1e
Steven Goff <steveng9@gmail.com>
2017-11-05 13:12:10 -0600

    Wire up welcome UI

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,4 +1,5 @@
 /*
+ * @flow
  * HomePage
  *
  * This is the first thing users see of our App, at the '/' route
@@ -10,15 +11,78 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import {
+  Input,
+  Page,
+  Toolbar,
+  Button,
+} from 'react-onsenui';
+import ons from 'onsenui';
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+type State = {
+  teamName: string,
+  roomCode: string,
+}
+export default class HomePage extends React.PureComponent<void, State> { // eslint-disable-line react/prefer-stateless-function
+
+  constructor(props: void) {
+    super(props);
+    this.state = {
+      teamName: '',
+      roomCode: '',
+    };
+    (this: any).handleRoomCodeChange = this.handleRoomCodeChange.bind(this);
+    (this: any).handleTeamNameChange = this.handleTeamNameChange.bind(this);
+  }
+
+  handleTeamNameChange(e: SyntheticInputEvent<*>) {
+    this.setState({ teamName: e.target.value });
+  }
+
+  handleRoomCodeChange(e: SyntheticInputEvent<*>) {
+    this.setState({ roomCode: e.target.value });
+  }
+
+
+  renderToolbar() {
+    return (
+      <Toolbar>
+        <div className="center">Trivia</div>
+      </Toolbar>
+    );
+  }
+
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <Page renderToolbar={this.renderToolbar}>
+        <section style={{ textAlign: 'center' }}>
+          <p>
+            <Input
+              modifier="underbar"
+              float
+              placeholder="Team Name"
+              value={this.state.teamName}
+              onChange={this.handleTeamNameChange}
+            />
+          </p>
+          <p>
+            <Input
+              modifier="underbar"
+              float
+              placeholder="Game Room Code"
+              value={this.state.roomCode}
+              onChange={this.handleRoomCodeChange}
+            />
+          </p>
+          <Button
+            onClick={
+            () => {
+              ons.notification.alert('Hello world!');
+            }
+          }
+          >Join</Button>
+        </section>
+      </Page>
     );
   }
 }
