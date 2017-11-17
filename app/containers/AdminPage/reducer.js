2922381a639796021ff5b6e46011d2400ccda6b9
Steven Goff <steveng9@gmail.com>
2017-11-17 14:22:19 -0600

    Create admin pages at /admin

---
+++ app/containers/AdminPage/reducer.js
@@ -0,0 +1,23 @@
+/*
+ *
+ * AdminPage reducer
+ *
+ */
+
+import { fromJS } from 'immutable';
+import {
+  DEFAULT_ACTION,
+} from './constants';
+
+const initialState = fromJS({});
+
+function adminPageReducer(state = initialState, action) {
+  switch (action.type) {
+    case DEFAULT_ACTION:
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default adminPageReducer;
