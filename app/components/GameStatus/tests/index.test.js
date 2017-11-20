67ce37dad676e5ba737979f8a8c4da96e778c847
Steven Goff <steveng9@gmail.com>
2017-11-19 18:03:47 -0600

    New component to make rendering various game statuses easier on the pages

---
+++ app/components/GameStatus/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import GameStatus from '../index';
+
+describe.skip('<GameStatus />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
