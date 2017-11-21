5ea1740e9d598b9bff2da43e26866c0cdefb9016
Steven <steveng9@gmail.com>
2017-11-20 19:34:40 -0600

    Implement levenshtein for fuzzy matching (#30)

--- functions/index.js
+++ functions/index.js
@@ -3,22 +3,19 @@
 const functions = require('firebase-functions');
 /* eslint-enable import/no-unresolved */
 
+const isMatch = require('./utils').isMatch;
 
-exports.makeUppercase = functions.database.ref('/scores/{gameKey}/playerKey/{gameRound}/response')
+exports.autoScore = functions.database.ref('/scores/{gameKey}/playerKey/{gameRound}/response')
 .onWrite((event) => {
   const response = event.data.val();
-  // console.log('Answer: ', answer);
   const { gameKey, gameRound } = event.params;
-  // console.log('GameKey: ', gameKey);
-  // console.log('GameRound: ', gameRound);
 
   event.data.ref.root.child('games').child(gameKey).child('questions').child(gameRound).once('value').then((snapshot) => {
     const question = snapshot.val();
 
-    // return event.data.ref.parent.child('uppercase').set(uppercase);
     return event.data.ref.parent.update({
       originalQuestion: question,
-      isCorrect: question.answer.toUpperCase() === response.toUpperCase(),
+      isCorrect: isMatch(response, question.answer),
     });
   });
 });
