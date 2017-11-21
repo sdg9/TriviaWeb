fd4ea2fb64e9ee44c4d22062b3966e400e3d1752
Steven Goff <steveng9@gmail.com>
2017-11-20 19:37:41 -0600

    Remove flow from functions so it works on server with no transpiling

--- functions/utils.js
+++ functions/utils.js
@@ -1,14 +1,10 @@
-/**
- * @flow
- */
-
 const levenshtein = require('fast-levenshtein');
 
 function compare(actual, expected) {
   return levenshtein.get(actual, expected, { useCollator: true });
 }
 
-exports.isMatch = function isMatch(actual: string, expected: string) {
+exports.isMatch = function isMatch(actual, expected) {
   if (expected.length > 8) {
     return compare(actual, expected) <= 2;
   } else if (expected.length > 1) {
